3ab2dcd17dbac96e56960bff164ed904f62430e8
Lana <Shuvaeva.lana@gmail.com>
2020-04-15 20:01:41 -0700

    Task #3 Pangram

--- app.js
+++ app.js
@@ -9,9 +9,9 @@
 //Otherwise: false
 
 function isValidPassword(password, username) {
-    const valid = ![...password].includes(' ') 
-                  && password.length > 8
-                  && !password.includes(username);
+    const valid = ![...password].includes(' ')
+        && password.length > 8
+        && !password.includes(username);
     return valid ? true : false;
 }
 
@@ -23,9 +23,32 @@ function isValidPassword(password, username) {
 // Task #2
 // Write a function to find the average value in an array of numbers
 
-    function avg(arr) {
-        return arr.reduce((x, y) => x + y) / arr.length;
-    };
+function avg(arr) {
+    return arr.reduce((x, y) => x + y) / arr.length;
+};
 
-console.log(avg([0,50])); //25
-console.log(avg([75,76,80,95,100])); //85.2
+// console.log(avg([0,50])); //25
+// console.log(avg([75,76,80,95,100])); //85.2
+
+
+// Task #3
+// A pangram is a sentence that contains every letter of the alphabet, like:
+//"The quick brown fox jumps over the lazy dog"
+
+// Write a function called isPangram, which checks to see if a given sentence 
+//contains every letter of the alphabet.  Make sure you igore string casing!
+
+function isPangram(str) {
+    const strLowerCased = str.toLowerCase();
+    for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+        if (!strLowerCased.includes(char)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// console.log(isPangram('The five boxing wizards jump quickly')); //true
+// console.log(isPangram('The five boxing wizards jump quick')); //false
+
+
