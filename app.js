02eae4f92ef0c0ec77cfbead0bfb752c5d91a1da
Lana <Shuvaeva.lana@gmail.com>
2020-04-15 20:35:06 -0700

    Task #4 Card Picker

--- app.js
+++ app.js
@@ -52,3 +52,25 @@ function isPangram(str) {
 // console.log(isPangram('The five boxing wizards jump quick')); //false
 
 
+//Task #4
+// Write a getCard() function which returns a random playing card object, like:
+// 		{
+// 			value: 'K'
+// 			suit: 'clubs'
+// 		}
+//Pick a random value from:
+//----1,2,3,4,5,6,7,8,9,10,J,Q,K,A
+//Pick a random suit from:
+//----clubs,spades, hearts, diamonds
+//Return both in an object
+
+function getCard() {
+    const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+    const picker = arr => arr[Math.floor(Math.random() * arr.length)];
+
+    return { value: picker(values), suit: picker(suits) };
+}
+
+console.log(getCard());
+
