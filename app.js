e7b4dd638fbd81af39175909b95a9e8d3ac166ae
Lana <Shuvaeva.lana@gmail.com>
2020-04-15 19:15:36 -0700

    Task #2: avg value in an array of nums

--- app.js
+++ app.js
@@ -15,8 +15,17 @@ function isValidPassword(password, username) {
     return valid ? true : false;
 }
 
-
 // console.log(isValidPassword('89Fjj1nms', 'dogLuvr'));  //true
 // console.log(isValidPassword('dogLuvr123!', 'dogLuvr')) //false
 // console.log(isValidPassword('hello1', 'dogLuvr')) //false
 
+
+// Task #2
+// Write a function to find the average value in an array of numbers
+
+    function avg(arr) {
+        return arr.reduce((x, y) => x + y) / arr.length;
+    };
+
+console.log(avg([0,50])); //25
+console.log(avg([75,76,80,95,100])); //85.2
